9759f6bf1bb88ee33205d2d5e98a2ce6d25cf76a
Jess <notadeveloperemail@protonmail.com>
2024-12-23 21:48:02 -0800

    pushing new tests

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,7 +5,7 @@ import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     uint8 public constant DECIMALS = 8;
-    int256 public constant INITIAL_PRICE = 2000e8;
+    int256 public constant INITIAL_PRICE = 3200 * 10 ** 8;
     NetworkConfig public activeNetworkConfig;
 
     struct NetworkConfig {
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -6,22 +6,54 @@ import {DeployFundMe} from "../script/DeoployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 1 ether;
+    uint256 constant STARTING_BALANCE = 100 ether;
 
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
-    function testMinimumDollarIsFive() public view {
-        assertEq(fundMe.MINIMUM_USD(), 5e18);
-    }
+    // function testMinimumDollarIsFive() public view {
+    //     assertEq(fundMe.MINIMUM_USD(), 5e18);
+    // }
 
-    function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), msg.sender);
-    }
+    // function testOwnerIsMsgSender() public view {
+    //     assertEq(fundMe.i_owner(), msg.sender);
+    // }
 
-    function testPriceFeedVersionIsAccurate() public view {
-        uint256 version = fundMe.getVersion();
-        assertEq(version, 4);
+    // function testPriceFeedVersionIsAccurate() public view {
+    //     uint256 version = fundMe.getVersion();
+    //     assertEq(version, 4);
+    // }
+
+    // function testFundFailsWithoutEnoughETH() public {
+    //     vm.expectRevert();
+    //     fundMe.fund();
+    // }
+
+    // function testPriceFeedIsWorking() public view {
+    //     (, int256 price, , , ) = fundMe.getPriceFeed().latestRoundData();
+    //     assertGt(price, 0); // Price should be greater than 0
+    // }
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+
+        // Log min USD requirement
+        console.log("Minimum USD required:", fundMe.MINIMUM_USD());
+
+        // Log ETH price from feed
+        (, int256 price, , , ) = fundMe.getPriceFeed().latestRoundData();
+        console.log("ETH/USD price from feed:", uint256(price));
+
+        // Log the value we're sending
+        console.log("Sending ETH value:", SEND_VALUE);
+
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
